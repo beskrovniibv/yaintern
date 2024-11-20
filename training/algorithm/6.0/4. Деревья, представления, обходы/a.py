a13fd0d10bfb125c9d042dffbc104098ea56cf49
boris <beskrovniibv@gmail.com>
2024-11-20 13:41:28 +0000

    Полное решение задачи `A`

---
+++ training/algorithm/6.0/4. Деревья, представления, обходы/a.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+
+# https://contest.yandex.ru/contest/66795/problems/A/
+
+# A. Родословная: подсчет уровней
+
+tree = {}
+heights = {}
+
+def get_height(key):
+    if key in heights:
+        return heights[key]
+    if key not in tree.keys():
+        heights[key] = 1
+        return 1
+    height = get_height(tree[key]) + 1
+    heights[key] = height
+    return height
+
+
+n = int(input())
+
+for _ in range(n - 1):
+    child, parent = input().split()
+    node = tree.get(parent)
+    tree[child] = parent
+
+root = (set(tree.values()) - set(tree.keys())).pop()
+
+heights[root] = 0
+
+for node in tree.keys():
+    get_height(node)
+
+for key in sorted(heights.keys()):
+    print(key, heights[key])
